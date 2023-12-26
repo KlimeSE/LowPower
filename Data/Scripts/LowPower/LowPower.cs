400c857292dfc9396a9e094c52da3685bc2704d7
KlimeSE <68099172+KlimeSE@users.noreply.github.com>
2023-12-26 18:28:43 +0000

    Initial commit

---
+++ Data/Scripts/LowPower/LowPower.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using Sandbox.Game;
+using Sandbox.ModAPI;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRageMath;
+
+
+namespace klime.LowPower
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
+    public class LowPower : MySessionComponentBase
+    {
+        public override void Init(MyObjectBuilder_SessionComponent sessionComponent)
+        {
+        }
+
+        public override void UpdateAfterSimulation()
+        {
+
+        }
+        protected override void UnloadData()
+        {
+
+        }
+    }
+}
